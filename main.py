a3975490400fd25fbbe344a36804c9b221ca8b22
Aekanunpr <69186367+Aekanunpr@users.noreply.github.com>
2021-03-23 16:52:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
 from fastapi import FastAPI
 import uvicorn
 
@@ -9,7 +16,7 @@ app = FastAPI()
 @app.get("/cut")
 def read_root(text:str):
     TEXT = text.lower()
-    list_word=TEXT.split()
-    #stop_words = set(stopwords.words('english'))
-    #filter_sentence=[word for word in list_word if not word in list(stop_words)]
-    return {"text": str(list_word)}#filter_sentence}
+    list_word=word_tokenize(TEXT)
+    stop_words = set(stopwords.words('english'))
+    filter_sentence=[word for word in list_word if not word in list(stop_words)]
+    return {"text": filter_sentence}
