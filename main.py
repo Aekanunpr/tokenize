33ba9d7b8eb16a779a043279e54cd001971e76b3
Aekanunpr <69186367+Aekanunpr@users.noreply.github.com>
2021-03-23 16:14:24 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from fastapi import FastAPI
+import uvicorn
+
+app = FastAPI()
+
+
+
+
+@app.get("/cut")
+def read_root(text:str):
+    TEXT = text.lower()
+    list_word=word_tokenize(TEXT)
+    stop_words = set(stopwords.words('english'))
+    filter_sentence=[word for word in list_word if not word in list(stop_words)]
+    return {"text": filter_sentence}
